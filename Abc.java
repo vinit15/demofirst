1cc84d4d8a118a430423e18c7fcdcee4e7c8ab75
vinit15 <vinitamolkale@gmail.com>
2022-03-15 15:33:34 +0530

    Altered Abc.java and constructed Xyz.java

--- Abc.java
+++ Abc.java
@@ -1,5 +1,10 @@
 public class Abc{
 	public static void main(String args[]){
 		System.out.println("My first line of code");
+		
+		for(int i=1;i<=10;i++){
+			System.out.println("Hello");
+		}
+
 	}
 }
---
+++ Xyz.java
@@ -0,0 +1,11 @@
+public class Xyz{
+	private int x;
+	private String str;
+
+	public void setStr(){	
+	}
+	public getStr(){
+		return str;
+	}
+
+}
